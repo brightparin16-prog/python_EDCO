39eb17b2fee49cb98b434a496a1ef3633166ef37
brightparin16-prog <bright.parin16@gmail.com>
2025-08-30 10:54:02 +0700

    Test

--- week03/rotation.py
+++ week03/rotation.py
@@ -1,15 +1,23 @@
-import cv2
-import numpy as np
+# import cv2
+# import numpy as np
+# import matplotlib.pyplot as plt
 
+# print(np.__version__)
 
-print(np.__version__)
+# img = cv2.imshow("C:/Users/Asus/Desktop/python-couse2/WRG-championship-logo-new.png")
 
-img = cv2.imshow("C:/Users/Asus/Desktop/python-couse2/WRG-championship-logo-new.png")
-
-
-# degree = 90
+# degree = [15, 45, , 90, 180]
 # rotation_image = []
-# cv2.imshow("Rotation", img)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# center = (width // 2 height // 2)
+# # for loop use to know about circle round of data
+# for x in degree:
+    rotation_matrix = cv2.getRotationMatrix2D()
+
+# rotation = cv2.warpAffine(img, rotation)
+# # degree = 90
+# # rotation_image = []
+# # cv2.imshow("Rotation", img)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
